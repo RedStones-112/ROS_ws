ea1ff457805df6dfc96eceea305f67797ba06ff2
rds <wogur21c@gmail.com>
2024-04-15 18:33:47 +0900

    cpp pkg start

---
+++ build/first_package_cpp/CMakeFiles/sub.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/rds/Desktop/git_ws/ROS_ws/src/first_package_cpp/src/my_subscriber.cpp" "CMakeFiles/sub.dir/src/my_subscriber.cpp.o" "gcc" "CMakeFiles/sub.dir/src/my_subscriber.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/first_package_cpp/CMakeFiles/sub.dir/src/my_subscriber.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/sub.dir/src/my_subscriber.cpp.o: \
+ /home/rds/Desktop/git_ws/ROS_ws/src/first_package_cpp/src/my_subscriber.cpp \
+ /usr/include/stdc-predef.h
